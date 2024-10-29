48d201fdd537b30fc9af1f8f3b7fcbf11e1c577e
ikemefuna <ikemefunaonubogu55@gmail.com>
2024-10-29 10:32:42 +0100

    feat: update reset export credits to reset credits in prod

--- resetExportCredits/index.js
+++ resetExportCredits/index.js
@@ -1,22 +1,40 @@
 const mysql = require('mysql2')
 const knex = require('knex')
 
-const connection = {
+// Configure connections to both staging and production db
+// ENVs are on aws lambda
+const devDBConnection = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 }
 
-const db = knex({
+const prodDBConnection = {
+    host: process.env.PROD_DB_HOST,
+    user: process.env.PROD_DB_USER,
+    password: process.env.DB_PASSWORD, // same password as staging db
+    database: process.env.PROD_DB_DATABASE
+}
+
+const devDB = knex({
     client: 'mysql2',
-    connection,
+    connection: devDBConnection,
+});
+
+const prodDB = knex({
+    client: 'mysql2',
+    connection: prodDBConnection,
 });
 
 module.exports.handler = async (event) => {
     try {
         console.log("Resetting export credits...")
-        await db('directory_export_credits').where('credits', '<', 10).update({credits: 10});
+
+        // update export credits in both staging and production
+        await devDB('directory_export_credits').where('credits', '<', 10).update({credits: 10});
+        await prodDB('directory_export_credits').where('credits', '<', 10).update({credits: 10});
+
         const response = {
           success: true
         };
